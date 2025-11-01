27e75e85b49843fecdc375b5f2fb3c15a6904ab1
CodexAde <adarshrpandey10@gmail.com>
2025-11-02 01:23:32 +0530

    Add skeleton to registering page adn convet

--- frontend/Service/UserService.jsx
+++ frontend/Service/UserService.jsx
@@ -1,24 +1,24 @@
 export const RegisterService = async (user) => {
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    const url = new URL(`${backendUrl}/api/users/register`);
-    const formData = new FormData();
-    formData.append("name", user.name);
-    formData.append("email", user.email);
-    formData.append("password", user.password);
-    formData.append("language", user.language);
-    formData.append("avatar", user.avatar);
-       try {
-      const res = await fetch(url, {
+  const url = new URL(`${backendUrl}/api/users/register`);
+  const formData = new FormData();
+  formData.append("name", user.name);
+  formData.append("email", user.email);
+  formData.append("password", user.password);
+  formData.append("language", user.language);
+  formData.append("avatar", user.avatar);
+  try {
+    const res = await fetch(url, {
       method: "POST",
       body: formData,
-      });
+    });
 
-      const response = await res.json();
-      console.log("Response:", response);
-      return response;
-    } catch (err) {
-      console.error("Error:", err);
-      alert("Something went wrong!");
-    }
+    const response = await res.json();
+    console.log("Response:", response);
+    return response;
+  } catch (err) {
+    console.error("Error:", err);
+    alert("Something went wrong!");
+  }
 }
